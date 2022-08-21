7df741ca6aea6c3f9773d52a5decff958455a79a
=libro1115 <libro.1115@gmail.com>
2022-08-22 08:41:49 +0900

    各ファイル作成

---
+++ structs/src/remining_money/mod.rs
@@ -0,0 +1,11 @@
+#![allow(dead_code)]
+mod module;
+
+struct ReminingMoney{
+
+}
+
+#[test]
+fn remining_money_test(){
+    
+}
---
+++ structs/src/remining_money/module.rs
@@ -0,0 +1,12 @@
+#![allow(dead_code)]
+use super::*;
+
+//public
+impl ReminingMoney{
+
+}
+
+//protected
+impl ReminingMoney{
+    
+}
