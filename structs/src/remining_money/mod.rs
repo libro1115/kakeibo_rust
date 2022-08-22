de980f485c9c6471d94a11b45c23124460f47d92
=libro1115 <libro.1115@gmail.com>
2022-08-22 09:34:54 +0900

    メンバ作成

--- structs/src/remining_money/mod.rs
+++ structs/src/remining_money/mod.rs
@@ -1,11 +1,14 @@
 #![allow(dead_code)]
 mod module;
-
+use std::collections::BTreeMap;
+use super::cash_history::{CashHistory, Badget};
+use super::day::Day;
 struct ReminingMoney{
-
+    logs:BTreeMap<Day, CashHistory>,
+    remining_money:BTreeMap<Badget,u64>,
 }
 
 #[test]
 fn remining_money_test(){
-    
+
 }
