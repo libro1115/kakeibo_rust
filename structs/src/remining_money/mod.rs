3219988c821ed630c1334693cdba58f49a16f5c2
=libro1115 <libro.1115@gmail.com>
2022-08-24 18:07:28 +0900

    資金操作ログの実装

--- structs/src/remining_money/mod.rs
+++ structs/src/remining_money/mod.rs
@@ -5,10 +5,9 @@ use super::cash_history::{CashHistory, Badget};
 use super::day::Day;
 struct ReminingMoney{
     logs:BTreeMap<Day, Vec<CashHistory>>,
-    remining_money:HashMap<Badget,u64>,
+    remining_money:HashMap<Badget,i128>,
 }
 
 #[test]
 fn remining_money_test(){
-
 }
