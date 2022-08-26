ac15bca9a0454e3c736a2f090e55f9d439b1eb22
=libro1115 <libro.1115@gmail.com>
2022-08-26 21:54:20 +0900

    テストコード

--- structs/src/remining_money/mod.rs
+++ structs/src/remining_money/mod.rs
@@ -10,4 +10,57 @@ struct ReminingMoney{
 
 #[test]
 fn remining_money_test(){
+    let mut rem = ReminingMoney::new();
+    let day = Day::new(2022, 1, 1);
+    let log1 = CashHistory::income(1000, 
+                                                day,
+                                                 Badget::Monthly, 
+                                                 String::new());
+    let log2 = CashHistory::income(10000,
+                                                 day,
+                                                Badget::Weekly,
+                                                String::new());
+     
+    let log3 = CashHistory::spending(5000,
+                                                 day,
+                                                Badget::Weekly,
+                                                String::new());
+     let log4 = CashHistory::spending(100000,
+                                                 day,
+                                                Badget::Monthly,
+                                                String::new());
+    
+    rem.push(log1);
+    assert_eq!(rem.get_remining(Badget::Monthly), 1000);
+    assert_eq!(rem.get_remining(Badget::Weekly),0);
+    rem.push(log2);
+    assert_eq!(rem.get_remining(Badget::Monthly), 1000);
+    assert_eq!(rem.get_remining(Badget::Weekly),10000);
+    rem.push(log3);
+    assert_eq!(rem.get_remining(Badget::Monthly), 1000);
+    assert_eq!(rem.get_remining(Badget::Weekly),5000);
+    rem.push(log4);
+    assert_eq!(rem.get_remining(Badget::Monthly), -99000);
+    assert_eq!(rem.get_remining(Badget::Weekly),5000);
+    match rem.get_logs(day){
+        Some(v)=>{
+            assert_eq!(v.len(), 4);
+        },
+        None=>{panic!("err 49");}
+    }
+    match rem.get_logs(day){
+       Some(v)=>{
+           assert_eq!(v.len(), 4);
+       },
+       None=>{panic!("err 55");}
+    }
+    println!("XXX");
+    match rem.get_logs(Day::new(2021,1,1)){
+        Some(v)=>{
+            println!("A");
+            if v.len() != 0{panic!("err 61");}
+            panic!("err 62");
+        },
+        None=>{println!("B");}
+    }
 }
