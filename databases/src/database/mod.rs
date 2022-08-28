e0fd355db78bd04583869ead5b6d7c07ca636f95
=libro1115 <libro.1115@gmail.com>
2022-08-28 21:16:00 +0900

    データベースの生成

--- databases/src/database/mod.rs
+++ databases/src/database/mod.rs
@@ -1,7 +1,15 @@
 #![allow(dead_code)]
 mod module;
-
+use rusqlite::{Connection}; 
 
 
 pub struct Database{
+    access_point:Connection,
+}
+
+
+#[test]
+fn database_test(){
+    let db = Database::connect();
+    db.create_table();
 }
--- databases/src/database/module.rs
+++ databases/src/database/module.rs
@@ -1,12 +1,47 @@
 use super::Database;
-use sqlite::{};
+use rusqlite::{Connection, params};
 
+
+const DB_PATH:&str = "./kakeibo_rust.sqlite3";
 //public
 impl Database {
-    
+    ///データベースと接続
+    pub fn connect()->Self{
+        Database{access_point: Self::connect_database()}
+    }
 }
 
 //protected
 impl  Database {
-    
+
+    ///データベースがなければ生成して接続
+    fn connect_database()->Connection{
+       match Connection::open(DB_PATH){
+            Ok(i)=>{
+                return i;
+            },
+            Err(_)=>{
+                panic!("database not found and can't create error")
+            }
+        } 
+    }
+    ///テーブルの作成
+    pub(crate)fn create_table(&self){
+        match self.access_point.execute(
+            "CREATE TABLE users (
+                                            year    INTEGER,
+                                            month   INTEGER,
+                                            day     INTEGER,
+                                            usage   INTEGER,
+                                            expenses INTEGER,
+                                            badget  INTEGER,
+                                            tag     INTEGER,
+                                            memo    TEXT
+            )",
+            (),
+        ){
+            Ok(_)=>{},
+            Err(_)=>{println!("create table error");}
+        };
+    }
 }
