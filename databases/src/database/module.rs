159a4340777a5c4a4831bf98cd3c0970398a2f11
=libro1115 <libro.1115@gmail.com>
2022-09-01 23:13:43 +0900

    データのロード

--- databases/src/database/module.rs
+++ databases/src/database/module.rs
@@ -1,6 +1,8 @@
+use std::{collections::BTreeMap};
+
 use super::Database;
-use rusqlite::{Connection, Error};
-use structs::{cash_history::CashHistory};
+use rusqlite::{Connection, Error, params};
+use structs::{cash_history::{CashHistory, Usage, Badget}, date::Date};
 
 const DB_PATH:&str = "./kakeibo_rust.sqlite3";
 //public
@@ -15,6 +17,53 @@ impl Database {
         self.access_point.execute(format!("INSERT INTO {} VALUES($1,$2,$3,$4,$5,$6,$7,$8,?9)"
         ,&self.user).as_str(),         
        Self::create_log_params(&log))
+    }
+    ///ログをデータベースから読み込み
+    pub fn load_logs(&self, start_date:&Date, end_date:&Date)
+    ->Result<BTreeMap<Date,CashHistory>,Error>
+    {
+        let sql = format!("SELECT * FROM {} WHERE ce >= {}  AND ce < {}", 
+                        &self.user, 
+                        &start_date.days_from_ce(),
+                        &end_date.days_from_ce()
+                    );
+        let mut map:BTreeMap<Date, CashHistory> = BTreeMap::new();
+        let mut stmt = self.access_point.prepare(
+            sql.as_str()
+            )?;
+            let person = stmt.query_map(params![], |row|{
+            Ok({
+                let ce:i32= row.get(0)?;
+                let usage:Usage = Usage::from_int(row.get(4)?);
+                let expenses:i64 = row.get(5)?;
+                let badget = Badget::from_int(row.get(6)?);
+                let tag:u64 = row.get(7)?;
+                let memo:String = row.get(8)?;
+                
+                println!("ce:{}",ce);
+
+                let date = Date::from_ce(ce);
+                let mut log:CashHistory;
+                if  usage == Usage::Income{   
+                    log = CashHistory::income(expenses as u64, 
+                                        Date::from_ce(ce), 
+                                        badget, 
+                                        memo);                            
+                }
+                else {
+                    log = CashHistory::spending(-expenses as u64, 
+                                            Date::from_ce(ce), 
+                                            badget, 
+                                            memo);
+                }
+                log.set_tag(tag);
+                map.insert(date, log);
+            })
+        }).unwrap();
+        for p in person{
+            println!("{:?}",p);
+        }
+        Ok(map)
     }
 }
 
